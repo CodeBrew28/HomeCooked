72fc15da10846ce8e517faad65ddbca5bdad87b0
Jared Royer <jarroyer2@gmail.com>
2018-11-25 16:10:32 -0600

    Added payment screen

---
+++ app/src/main/java/com/example/ariamalkani/homecooked/PaymentActivity.java
@@ -0,0 +1,18 @@
+package com.example.ariamalkani.homecooked;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedState) {
+        super.onCreate(savedState);
+
+        setContentView(R.layout.activity_payment);
+
+    }
+
+
+}
--- app/src/main/java/com/example/ariamalkani/homecooked/ScheduleActivity.java
+++ app/src/main/java/com/example/ariamalkani/homecooked/ScheduleActivity.java
@@ -3,12 +3,15 @@ package com.example.ariamalkani.homecooked;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class ScheduleActivity extends AppCompatActivity {
 
+    Button attendButton;
     ImageView thumbnail;
     TextView chefNameView;
     TextView verifiedView;
@@ -22,6 +25,7 @@ public class ScheduleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
         populateData(getIntent());
+        setButton();
     }
 
     private void populateData(Intent intent) {
@@ -58,5 +62,15 @@ public class ScheduleActivity extends AppCompatActivity {
         locationView = findViewById(R.id.location);
         text = "Location: " + intent.getStringExtra("LOCATION");
         locationView.setText(text);
+    }
+
+    private void setButton() {
+        attendButton = findViewById(R.id.attend_button);
+        attendButton.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), PaymentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
